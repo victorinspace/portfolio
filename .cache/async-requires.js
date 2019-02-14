b4575eb2e0d573e3de2b215c1e2deca23b88537c
Casper The Beast <alexxx@Caspers-MacBook-Pro.local>
2019-02-13 23:40:23 -0800

    Moving to GatsbyJS

---
+++ .cache/async-requires.js
@@ -0,0 +1,10 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-pages-about-css-modules-js": () => import("/Users/alexxx/Documents/work/portfolio/victorinspace.github.io/src/pages/about-css-modules.js" /* webpackChunkName: "component---src-pages-about-css-modules-js" */),
+  "component---src-pages-index-js": () => import("/Users/alexxx/Documents/work/portfolio/victorinspace.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/alexxx/Documents/work/portfolio/victorinspace.github.io/.cache/data.json")
+
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => (
+  <header class="introduction">
+    <h1>Hi, I'm Victor.</h1>
+    <h1>I'm a Front-End Developer.</h1>
+  </header>
+);

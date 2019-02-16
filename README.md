db185b892ca5079c585c097f8f55d1a6ba2e2612
Casper The Beast <alexxx@Caspers-MacBook-Pro.local>
2019-02-15 19:54:09 -0800

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Victor Velasquez | Portfolio
-
-This repo holds my portfolio, a one stop shop to everything I do online. 🎉
-
-## License
-
-MIT © Victor Velasquez
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default () => (
-  <header class="introduction">
-    <h1>Hi, I'm Victor.</h1>
-    <h1>I'm a Front-End Developer.</h1>
-  </header>
-);

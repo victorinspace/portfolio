c04e70f2d8f55019f9c3adcb39416106f46c5a9a
Victor Velasquez <victorv24@gmail.com>
2019-02-21 07:10:36 -0800

    Updates

--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---src-pages-index-js": () => import("/Users/alexxx/Documents/work/portfolio/victorinspace.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
-}
-
-exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/alexxx/Documents/work/portfolio/victorinspace.github.io/.cache/data.json")
-

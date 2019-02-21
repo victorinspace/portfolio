ea384a259ed19a0847efb0e47ddf9e637efa0c09
Victor Velasquez <victorv24@gmail.com>
2019-02-21 06:59:02 -0800

    new site build

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,6 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/alexxx/Documents/work/portfolio/victorinspace.github.io/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-index-js": () => import("/Users/alexxx/Documents/work/portfolio/victorinspace.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
